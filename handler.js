34334e48d5ce0a1fb497957e65bb37f79a9c591a
mamerisawesome <atmalmer23@gmail.com>
2018-01-01 23:34:58 +0800

    Updated handler readability

--- handler.js
+++ handler.js
@@ -1,12 +1,11 @@
 'use strict'
 
-module.exports = {
-    hello: (event, context, callback) => {
-        const response = {
-            foo: 'bar',
-            awe: process.env.AWESOME_VAR
-        }
-
-        callback(null, response)
+module.exports.hello = (event, context, callback) => {
+    const response = {
+        foo: 'bar',
+        awe: process.env.AWESOME_VAR,
+        secret: process.env.SECRET_VAR,
     }
+
+    callback(null, response)
 }
