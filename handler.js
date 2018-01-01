f0c5da4d6f74c6a9e7298ffb1839f0a9914c3640
mamerisawesome <atmalmer23@gmail.com>
2018-01-01 23:58:52 +0800

    updated handler

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
 'use strict'
 
+/**
+ * @description project init functions for testing purposes
+ */
 module.exports.hello = (event, context, callback) => {
     const response = {
         foo: 'bar',
@@ -8,4 +11,16 @@ module.exports.hello = (event, context, callback) => {
     }
 
     callback(null, response)
+}
+
+/**
+ * @description http methods
+ * @todo make this work for single file referencing
+ */
+module.exports.todos_http = {
+    post    : require('./todos/create'),
+    del     : require('./todos/delete'),
+    get     : require('./todos/get'),
+    get_all : require('./todos/list'),
+    put     : require('./todos/update'),
 }
