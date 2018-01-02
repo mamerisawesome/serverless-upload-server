3de4a4862beee5d384ee13c539ffef2ac0e4ad38
mamerisawesome <atmalmer23@gmail.com>
2018-01-02 22:40:07 +0800

    Added newline in todo EOF

--- todos/src/create.js
+++ todos/src/create.js
@@ -3,7 +3,7 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
-const rds = new AWS.RDS();
+const Todo = require('./../db/models/todo');
 
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
@@ -19,7 +19,7 @@ module.exports.create = (event, context, callback) => {
   }
 
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
+    TableName: process.env.TABLE_NAME,
     Item: {
       id: uuid.v1(),
       text: data.text,
@@ -30,23 +30,7 @@ module.exports.create = (event, context, callback) => {
   };
 
   // write the todo to the database
-  dynamoDb.put(params, (error) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the todo item.',
-      });
-      return;
-    }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
+  Todo.sync({force: true}).then(() => {
+    callback(null, Todo.create(params.Item));
   });
 };
