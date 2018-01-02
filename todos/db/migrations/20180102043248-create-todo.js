c001cf6e85cda4aafcdc13d248978d9fa0b26142
mamerisawesome <atmalmer23@gmail.com>
2018-01-02 12:43:28 +0800

    Added db configs

---
+++ todos/db/migrations/20180102043248-create-todo.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      checked: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: (new Date()),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: (new Date()),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Todos');
+  }
+};
---
+++ todos/db/seeders/20180102043504-demo-todo.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Todos', [{
+      text: 'This is the first todo.',
+      checked: false
+    },{
+      text: 'I had an amazing dream but I forgot about it.',
+      checked: false
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Todos', null, {});
+  }
+};
